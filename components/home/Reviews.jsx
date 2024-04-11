43e0502f877fdc14716bba7436f6afb5f32984d8
suhel <suhel9101@gmail.com>
2024-04-11 19:43:25 +0530

    update

--- components/home/Reviews.jsx
+++ components/home/Reviews.jsx
@@ -95,7 +95,7 @@ const Reviews = () => {
         <Slider {...settings} className="gap-5">
     {
       reviewsData.map((item, index)=>{
-        return    <div className="flex justify-center items-center gap-6">
+        return    <div className="flex justify-center items-center gap-6" key={`${index}`}>
             <div className="card">
         <div className="header">
           <div className="image" >
